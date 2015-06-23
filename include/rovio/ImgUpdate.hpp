9693b18c836a489483c3f0d57d7e874e00998170
AndreasJaeger <jaegera@student.ethz.ch>
2015-06-23 18:06:52 +0200

    Fix: Compute standard deviation from variance

--- include/rovio/ImgUpdate.hpp
+++ include/rovio/ImgUpdate.hpp
@@ -510,15 +510,14 @@ class ImgUpdate: public LWF::Update<ImgInnovation<typename FILTERSTATE::mtState>
     float medianDepthSF[mtState::nCam_] = {float(initDepth_)};
     std::vector<float> depthCollectionSF[mtState::nCam_];
     const float maxUncertaintyToDepthRatio = 0.3;
-    double depth, depthPlus, depthMinus, d_p, p_d, p_d_p, sigmaMax;
+    double depth, depthPlus, depthMinus, d_p, p_d, p_d_p, sigmaDep, sigmaMax;
     for (unsigned int i = 0; i < mtState::nMax_; i++) {
       if (filterState.mlps_.isValid_[i]) {
         const int camID = filterState.mlps_.features_[i].camID_;
         state.template get<mtState::_aux>().depthMap_.map(state.template get<mtState::_dep>(i), depth, d_p, p_d, p_d_p);
-        state.template get<mtState::_aux>().depthMap_.map(state.template get<mtState::_dep>(i) + cov(mtState::template getId<mtState::_dep>(i),mtState::template getId<mtState::_dep>(i))
-                                  , depthPlus, d_p, p_d, p_d_p);
-        state.template get<mtState::_aux>().depthMap_.map(state.template get<mtState::_dep>(i) - cov(mtState::template getId<mtState::_dep>(i),mtState::template getId<mtState::_dep>(i))
-                                  , depthMinus, d_p, p_d, p_d_p);
+        sigmaDep = std::sqrt(cov(mtState::template getId<mtState::_dep>(i),mtState::template getId<mtState::_dep>(i)));
+        state.template get<mtState::_aux>().depthMap_.map(state.template get<mtState::_dep>(i) + sigmaDep, depthPlus, d_p, p_d, p_d_p);
+        state.template get<mtState::_aux>().depthMap_.map(state.template get<mtState::_dep>(i) - sigmaDep, depthMinus, d_p, p_d, p_d_p);
         sigmaMax = std::max(std::abs(depthPlus - depth), std::abs(depthMinus - depth));
         if (sigmaMax/depth <= maxUncertaintyToDepthRatio) {
           depthCollectionSF[camID].push_back(depth);
