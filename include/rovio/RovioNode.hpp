ea68fc2d7ad4af18a83604d23611b32e1836d32c
Robostation <robostation@robostation>
2019-10-15 16:08:07 +0200

    Switch to mono8 for encoding.

--- include/rovio/RovioNode.hpp
+++ include/rovio/RovioNode.hpp
@@ -499,7 +499,7 @@ class RovioNode{
     // Get image from msg
     cv_bridge::CvImagePtr cv_ptr;
     try {
-      cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::TYPE_8UC1);
+      cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8);
     } catch (cv_bridge::Exception& e) {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
